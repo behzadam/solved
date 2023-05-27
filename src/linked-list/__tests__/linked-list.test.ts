057616bf18dd69f87124e808bcbb9d7f13ed91f9
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-27 05:09:11 +0330

    test: add delete linked list head

--- src/linked-list/__tests__/linked-list.test.ts
+++ src/linked-list/__tests__/linked-list.test.ts
@@ -79,6 +79,32 @@ describe("LinkedList", () => {
     expect(linkedList.toString()).toBe("");
   });
 
+  it("deletes linked list head", () => {
+    const linkedList = new LinkedList();
+
+    expect(linkedList.deleteHead()).toBeNull();
+
+    linkedList.append(1);
+    linkedList.append(2);
+
+    expect(linkedList.head?.toString()).toBe("1");
+    expect(linkedList.tail?.toString()).toBe("2");
+
+    const deletedNode1 = linkedList.deleteHead();
+
+    expect(deletedNode1.value).toBe(1);
+    expect(linkedList.toString()).toBe("2");
+    expect(linkedList.head?.toString()).toBe("2");
+    expect(linkedList.tail?.toString()).toBe("2");
+
+    const deletedNode2 = linkedList.deleteHead();
+
+    expect(deletedNode2.value).toBe(2);
+    expect(linkedList.toString()).toBe("");
+    expect(linkedList.head).toBeNull();
+    expect(linkedList.tail).toBeNull();
+  });
+
   it("deletes linked list tail", () => {
     const linkedList = new LinkedList();
 
@@ -91,21 +117,21 @@ describe("LinkedList", () => {
 
     const deletedNode1 = linkedList.deleteTail();
 
-    expect(deletedNode1.value).toBe(3);
+    expect(deletedNode1?.value).toBe(3);
     expect(linkedList.toString()).toBe("1,2");
     expect(linkedList.head?.toString()).toBe("1");
     expect(linkedList.tail?.toString()).toBe("2");
 
     const deletedNode2 = linkedList.deleteTail();
 
-    expect(deletedNode2.value).toBe(2);
+    expect(deletedNode2?.value).toBe(2);
     expect(linkedList.toString()).toBe("1");
     expect(linkedList.head?.toString()).toBe("1");
     expect(linkedList.tail?.toString()).toBe("1");
 
     const deletedNode3 = linkedList.deleteTail();
 
-    expect(deletedNode3.value).toBe(1);
+    expect(deletedNode3?.value).toBe(1);
     expect(linkedList.toString()).toBe("");
     expect(linkedList.head).toBeNull();
     expect(linkedList.tail).toBeNull();
