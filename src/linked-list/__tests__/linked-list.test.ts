510de791fa9d40c73bc256107c29c2deb75bb2f4
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-27 04:53:01 +0330

    test: add delete linked list tail test cases

--- src/linked-list/__tests__/linked-list.test.ts
+++ src/linked-list/__tests__/linked-list.test.ts
@@ -78,4 +78,36 @@ describe("LinkedList", () => {
     linkedList.delete(2);
     expect(linkedList.toString()).toBe("");
   });
+
+  it("deletes linked list tail", () => {
+    const linkedList = new LinkedList();
+
+    linkedList.append(1);
+    linkedList.append(2);
+    linkedList.append(3);
+
+    expect(linkedList.head?.toString()).toBe("1");
+    expect(linkedList.tail?.toString()).toBe("3");
+
+    const deletedNode1 = linkedList.deleteTail();
+
+    expect(deletedNode1.value).toBe(3);
+    expect(linkedList.toString()).toBe("1,2");
+    expect(linkedList.head?.toString()).toBe("1");
+    expect(linkedList.tail?.toString()).toBe("2");
+
+    const deletedNode2 = linkedList.deleteTail();
+
+    expect(deletedNode2.value).toBe(2);
+    expect(linkedList.toString()).toBe("1");
+    expect(linkedList.head?.toString()).toBe("1");
+    expect(linkedList.tail?.toString()).toBe("1");
+
+    const deletedNode3 = linkedList.deleteTail();
+
+    expect(deletedNode3.value).toBe(1);
+    expect(linkedList.toString()).toBe("");
+    expect(linkedList.head).toBeNull();
+    expect(linkedList.tail).toBeNull();
+  });
 });
