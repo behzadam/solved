444f74382bb94dfb4fb82a0906c8124bef22ec8d
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-27 17:29:35 +0330

    test: add reverse linked list test cases

--- src/linked-list/__tests__/linked-list.test.ts
+++ src/linked-list/__tests__/linked-list.test.ts
@@ -183,4 +183,27 @@ describe("LinkedList", () => {
     expect(node?.value?.value).toBe("test2");
     expect(linkedList.find({ key: 2, value: "test5" })).toBeNull();
   });
+
+  it("reverses linked list", () => {
+    const linkedList = new LinkedList();
+
+    // Add test values to linked list.
+    linkedList.append(1).append(2).append(3);
+
+    expect(linkedList.toString()).toBe("1,2,3");
+    expect(linkedList.head?.value).toBe(1);
+    expect(linkedList.tail?.value).toBe(3);
+
+    // Reverse linked list.
+    linkedList.reverse();
+    expect(linkedList.toString()).toBe("3,2,1");
+    expect(linkedList.head?.value).toBe(3);
+    expect(linkedList.tail?.value).toBe(1);
+
+    // Reverse linked list back to initial state.
+    linkedList.reverse();
+    expect(linkedList.toString()).toBe("1,2,3");
+    expect(linkedList.head?.value).toBe(1);
+    expect(linkedList.tail?.value).toBe(3);
+  });
 });
