9d8a0b44116a7abc4d1feb0af515d3961bf0aff6
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-27 04:49:53 +0330

    test: add delete test cases

--- src/linked-list/__tests__/linked-list.test.ts
+++ src/linked-list/__tests__/linked-list.test.ts
@@ -32,4 +32,50 @@ describe("LinkedList", () => {
     expect(linkedList.toString()).toBe("1,2,3");
     expect(linkedList.tail?.next).toBeNull();
   });
+
+  it("deletes node by value from linked list", () => {
+    const linkedList = new LinkedList();
+
+    expect(linkedList.delete(5)).toBeNull();
+
+    linkedList.append(1);
+    linkedList.append(1);
+    linkedList.append(2);
+    linkedList.append(3);
+    linkedList.append(3);
+    linkedList.append(3);
+    linkedList.append(4);
+    linkedList.append(5);
+
+    expect(linkedList.head?.toString()).toBe("1");
+    expect(linkedList.tail?.toString()).toBe("5");
+
+    const deletedNode = linkedList.delete(3);
+    expect(deletedNode?.value).toBe(3);
+    expect(linkedList.toString()).toBe("1,1,2,4,5");
+
+    linkedList.delete(3);
+    expect(linkedList.toString()).toBe("1,1,2,4,5");
+
+    linkedList.delete(1);
+    expect(linkedList.toString()).toBe("2,4,5");
+
+    expect(linkedList.head?.toString()).toBe("2");
+    expect(linkedList.tail?.toString()).toBe("5");
+
+    linkedList.delete(5);
+    expect(linkedList.toString()).toBe("2,4");
+
+    expect(linkedList.head?.toString()).toBe("2");
+    expect(linkedList.tail?.toString()).toBe("4");
+
+    linkedList.delete(4);
+    expect(linkedList.toString()).toBe("2");
+
+    expect(linkedList.head?.toString()).toBe("2");
+    expect(linkedList.tail?.toString()).toBe("2");
+
+    linkedList.delete(2);
+    expect(linkedList.toString()).toBe("");
+  });
 });
