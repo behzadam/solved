5168b04e155822b9832dc906cbe3dd7c4c5100ef
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-29 13:04:31 +0330

    feat: add HashTable get | set | delete functionalities

--- src/hash-table/HashTable.ts
+++ src/hash-table/HashTable.ts
@@ -1,20 +1,21 @@
 import LinkedList from "@/linked-list/linked-list";
+import LinkedListNode from "@/linked-list/linked-list-node";
+import { Nullable } from "@/types";
+import { Pair } from "@/types/pair";
 
-type Pair<TValue> = {
-  key: string | null;
-  value: TValue | null;
-};
 const defaultHashTableSize = 32;
 export default class HashTable<TValue = unknown> {
-  public buckets: Array<LinkedList<Pair<TValue>>>;
+  private _keys: Record<string, number>;
+  public buckets: LinkedList<Pair<TValue>>[];
 
   constructor(size = defaultHashTableSize) {
+    this._keys = {};
     this.buckets = new Array(size)
       .fill({ key: null, value: null } as Pair<TValue>)
       .map(() => new LinkedList<Pair<TValue>>());
   }
 
-  hash(key: string) {
+  private hash(key: string) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
       hash += key.charCodeAt(i);
@@ -22,18 +23,86 @@ export default class HashTable<TValue = unknown> {
     return hash % this.buckets.length;
   }
 
-  set(key: string, value: TValue) {
+  set(key: string, value: TValue): void {
     const keyHash = this.hash(key);
-    const node = this.buckets[keyHash].find({
-      condition: (nodeValue) => nodeValue?.key === key,
+    this._keys[key] = keyHash;
+    const bucket = this.buckets[keyHash];
+    const node = bucket.find({
+      condition: (nodeValue) => nodeValue.key === key,
     });
 
     if (!node) {
       // Insert new node.
-      this.buckets[keyHash].append({ key, value });
+      bucket.append({ key, value });
     } else {
       // Update value of existing node.
       node.value.value = value;
     }
+  }
+
+  /**
+   *
+   * @param key
+   * @returns deleted nod or null.
+   */
+  delete(key: string): Nullable<LinkedListNode<Pair<TValue>>> {
+    const keyHash = this.hash(key);
+    const bucket = this.buckets[keyHash];
+    delete this._keys[key];
+    const node = bucket.find({
+      condition: (nodeValue) => nodeValue?.key === key,
+    });
+
+    if (node) {
+      return bucket.delete(node.value);
+    }
+
+    return null;
+  }
+
+  /**
+   * Checks if key exists in bucket.
+   * @param key
+   * @returns true or false.
+   */
+  has(key: string): boolean {
+    return key in this._keys;
+  }
+
+  /**
+   * Returns value of key in bucket.
+   * @param key
+   * @returns value or null.
+   */
+  get(key: string): Nullable<TValue> {
+    const bucket = this.buckets[this.hash(key)];
+    const node = bucket.find({
+      condition: (nodeValue) => nodeValue.key === key,
+    });
+    return node ? node.value.value : undefined;
+  }
+
+  /**
+   * Returns all the keys.
+   * @returns array of keys.
+   */
+  getKeys(): string[] {
+    return Object.keys(this._keys);
+  }
+
+  /**
+   * Returns all hash table values.
+   * @returns array of values.
+   */
+  getValues(): TValue[] {
+    return this.buckets.reduce(
+      (values: TValue[], bucket: LinkedList<Pair<TValue>>) => {
+        const bucketValues = bucket
+          .toArray()
+          .map((linkedListNode) => linkedListNode.value.value);
+        return values.concat(bucketValues as TValue[]);
+      },
+      [] as TValue[]
+    );
   }
 }
