19ac977c2c1d1a8d52ce21d864e6cf10831aa2cc
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-06-04 02:32:29 +0330

    test: add reverse test cases

--- src/linked-list/__tests__/linded-list-refactoring.test.ts
+++ src/linked-list/__tests__/linded-list-refactoring.test.ts
@@ -196,4 +196,27 @@ describe("LinkedList Refactored", () => {
     expect(linkedList.head?.toString()).toBe("2");
     expect(linkedList.tail?.toString()).toBe("4");
   });
+
+  it("reverses linked list", () => {
+    const linkedList = new LinkedList<number>();
+
+    // Add test values to linked list.
+    linkedList.append(1).append(2).append(3);
+
+    expect(linkedList.toString()).toBe("1,2,3");
+    expect(linkedList.head?.value).toBe(1);
+    expect(linkedList.tail?.value).toBe(3);
+
+    // Reverse linked list.
+    linkedList.reverse();
+    expect(linkedList.toString()).toBe("3,2,1");
+    expect(linkedList.head?.value).toBe(3);
+    expect(linkedList.tail?.value).toBe(1);
+
+    // Reverse linked list back to initial state.
+    linkedList.reverse();
+    expect(linkedList.toString()).toBe("1,2,3");
+    expect(linkedList.head?.value).toBe(1);
+    expect(linkedList.tail?.value).toBe(3);
+  });
 });
