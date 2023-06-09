3f8202f622568ef814039fdfd1a9b826d91010b3
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-06-09 18:33:58 +0330

    chore: add comments to Queue class

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -1,6 +1,14 @@
 import LinkedList from "@/linked-list/linked-list";
 import { Nullable } from "@/types";
 
+/**
+ * A Queue is defined as a linear data structure that
+ * is open at both ends and the operations are performed
+ * in First In First Out (FIFO) order.
+ *
+ * This queue is based on the LinkedList because
+ * they are quite similar.
+ */
 export default class Queue<Element> {
   private list: LinkedList<Element>;
   constructor() {
